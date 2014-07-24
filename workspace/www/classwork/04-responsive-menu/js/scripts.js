f4f1ef039f1a36ebe8f6d07450b95a970b608ce3
webdesignchick <strawberryelle85@gmail.com>
2014-07-24 23:15:07 +0000

    05 bootstrap first commit

---
+++ workspace/www/classwork/04-responsive-menu/js/scripts.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+	var ww = document.body.clientWidth;	
+	
+	$(".toggleMenu").click(function(e){
+		e.preventDefault();
+		$(".nav").toggle();
+	})
+	
+	$(".nav li a").each(function() {
+        if ($(this).next().length > 0) {
+            $(this).addClass("parent");
+        };
+    })
+	
+	// adjustMenu();
+			if (ww < 800) {
+		$(".toggleMenu").css("display", "inline-block");
+		$(".nav").hide();
+		$(".nav li a") .click(function(){
+			$(this).parent("li").toggleClass("hover");
+		});
+		}
+		else {	
+		$(".toggleMenu").css("display", "none");
+		
+		$(".nav li").hover(function() {
+			$(this).addClass("hover");
+		}, function() {
+			$(this).removeClass("hover");
+		
+		});
+		}
+	
+	
+	});
+	
+	//function adjustMenu(){
+		
+
+	}
+	
+	
+	
+	
+	
+
+	
--- workspace/www/classwork/04.1-responsive-menu/js/scripts.js
+++ workspace/www/classwork/04.1-responsive-menu/js/scripts.js
@@ -1,8 +1,10 @@
+  var ww = document.body.clientWidth;  
 $(document).ready(function(){
-  var ww = document.body.clientWidth;
-  
+
   $(".toggleMenu").click(function(e) {
     e.preventDefault();
+    //add active class for styling
+    $(this).toggleClass('active');
     $(".nav").toggle();
   });
   
@@ -12,27 +14,59 @@ $(document).ready(function(){
     }
   });
   
-    if (ww < 800) {
+  adjusMenu();
+   
+ 
+});//end document.ready function
+
+//call function on page load and on windw resize
+var adjustMenu = function(){
+function adjustMenu() {
+   if (ww < 800) {
     $(".toggleMenu").css("display", "inline-block");
-    $(".nav").hide();
-    $(".nav li a").click(function() {
+
+     //add condition to show/hide button
+     if(!$".toggleMenu").hasClass("active"){
+       if () {
+       $(".nav").hide();
+       }
+       else {
+       $(".nav").show();
+       }
+       
+   
+ //   $(".nav li a").click(function(e) {
+       //unbind events on small screen
+       $(".nav li").unbind('mouseenter mouseleave');
+          $(".nav li a.parent").unbind('click').bind('click', function(e)){
+                                                     //aached to anchor element to prevent bubbling
+          
+      e.preventDefault();
       $(this).parent("li").toggleClass("hover");
     });
   } else {
     $("toggleMenu").css("display", "none");  
-    $(".nav li").hover(function() {
-      $(this).addClass("hover");
-    }, function() {
-      $(this).removeClass("hover");
-    });
-   } 
- 
-});//end document.ready function
+    //show nav on resize
+    $(".nav").show();
+   
+ //   $(".nav li").hover(function() {
+   //   $(this).addClass("hover");
+  //  }, function() {
+   //   $(this).removeClass("hover");
+  
+      $(".nav li").removeClass("hover");
+      $(".nav li a").unbind("click");
+      $(".nav li a").unbind('mouseenter mouseleave');.bind('mouseenter mouseleave', function(){
+        $(this).toggleClas('hover');
+        });
+    };
+                       
 
-
-
-
-
+$(window).bind ('resize orientationchange', function(){
+  ww = window.body.clientWidth;
+  adjustMenu();
+  
+});
 
 
 
